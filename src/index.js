2782721287c4e13299f4e9eb42811bb10f0abc34
Billi Ramirez <ramirezbilly42@outlook.com>
2019-06-07 10:57:32 -0600

    Adding plugings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
-import addImage from './add-image';
+import Heading from './components/heading/heading';
+
 
 const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+
+heading.render();
 helloWorldButton.render();
-// addImage();
+
+
+const ten = 210;
