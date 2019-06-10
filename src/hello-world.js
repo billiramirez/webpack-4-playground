28572af997c7f75d440877d792058d24ffa3e845
Billi Ramirez <ramirezbilly42@outlook.com>
2019-06-10 14:14:41 -0600

    Adding multiple entry point

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,5 +1,16 @@
-function helloWorld(){
-  console.log('hello');
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading';
+
+
+const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+
+heading.render();
+helloWorldButton.render();
+
+if(process.env.NODE_ENV === 'production'){
+  console.log('production');
+}else if(process.env.NODE_ENV === 'development'){
+  console.log('developtment');
 }
 
-export default helloWorld;
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,9 +4,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'hello-world': './src/hello-world.js',
+    'kiwi': './src/kiwi.js'
+  },
   output: {
-    filename: 'bundle-[contentHash].js',
+    filename: '[name]-[contentHash].js',
     path: path.resolve(__dirname, './dist'),
     publicPath: ''
   },
@@ -52,13 +55,22 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles-[contentHash].css'
+      filename: '[name]-[contentHash].css'
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      filename: 'hello-world.html',
+      chunks: ['hello-world'],
       title: 'Hello World',
-      template: 'src/index.hbs',
+      template: 'src/page-template.hbs',
       description: 'Some description'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'kiwi.html',
+      chunks: ['kiwi'],
+      title: 'Hello Kiwi',
+      template: 'src/page-template.hbs',
+      description: 'Kiwi description'
     })
   ]
 }
